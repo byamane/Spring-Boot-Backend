ba73624d4edfbfa9ea4039a3293a9d9ecbf76629
byamane <byamane21@gmail.com>
2022-07-26 15:34:08 -0500

    configure MYSQL database connection

--- studentsystem/src/main/java/com/byamane/studentsystem/StudentsystemApplication.java
+++ studentsystem/src/main/java/com/byamane/studentsystem/StudentsystemApplication.java
@@ -1,4 +1,4 @@
-package com.byamane.studentsystem;
+ package com.byamane.studentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
